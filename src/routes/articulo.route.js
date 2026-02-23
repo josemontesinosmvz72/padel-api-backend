a57bcdf89b680326ecbccf0856ade6358a28b980
Jose Montesinos <josemontesinosmvz@gmail.com>
2026-02-23 21:18:58 +0100

    Updated endpoint /all

--- src/routes/articulo.route.js
+++ src/routes/articulo.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const articuloController = require('../controllers/articulo.controller');
 const { route } = require("express/lib/application");
 
-router.get('/', articuloController.getAllArticulos);
+router.get('/all', articuloController.getAllArticulos);
 router.get('/paged', articuloController.getAllArticulos);
 router.get('/detail/:id', articuloController.getArticuloPorId);
 router.get('/categoria/:categoria', articuloController.getArticulosPorCategoria);

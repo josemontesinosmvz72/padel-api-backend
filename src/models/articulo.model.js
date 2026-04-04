e4bc2ee19ecb8fde91432801952629f48aacb470
Jose Montesinos <josemontesinosmvz@gmail.com>
2026-04-04 14:54:17 +0200

    subida backend

--- src/models/articulo.model.js
+++ src/models/articulo.model.js
@@ -3,22 +3,22 @@ const {Schema} = mongoose;
 
 const articuloSchema = new mongoose.Schema({
     nombre: { type: String, required: true },
-    imagen: { type: String, required: true }, // URL de la imagen
+    imagen: { type: String, required: true },
     descripcion: { type: String, required: true },
     categoria: {
         type: String,
         required: true,
         enum: ['palas', 'ropa', 'calzado', 'mochilas', 'accesorios']
     },
-    subcategoria: { type: String }, // ej: 'camisetas', 'pantalones', 'grips'
+    subcategoria: { type: String },
     marca: { type: String, required: true },
+    fechaLanzamiento: { type: Date },
     precioRebajado: { type: Number, required: true },
     precio: {type: Number, required: true},
-
-    // Campos opcionales según la categoría
-    talla: { type: String }, // Para ropa y calzado
+    
+    talla: { type: String },
     color: { type: String },
-    peso: { type: String }, // Para palas
+    peso: { type: String },
     stock: { type: Number, default: 0 },
 
 }, { timestamps: true, versionKey: false });

d16bd080032838f18fe56df8e9f597d004ee050b
Jose Montesinos <josemontesinosmvz@gmail.com>
2026-04-19 19:17:52 +0200

    - Actualización modelo a gemini-2.0 flash

--- src/routes/chat.route.js
+++ src/routes/chat.route.js
@@ -16,7 +16,7 @@ router.post('/message', async (req, res) => {
       parts: [{ text: m.text }]
     }));
 
-    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+    const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash' });
     const chatSession = model.startChat({ history });
     const result = await chatSession.sendMessage(message);
     const reply = result.response.text();

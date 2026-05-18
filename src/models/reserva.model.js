4675a9ef8941dc2a36f6151d002f4d8fc156bcd5
Jose Montesinos <josemontesinosmvz@gmail.com>
2026-05-18 19:57:44 +0200

    Subida backend

--- src/models/reserva.model.js
+++ src/models/reserva.model.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const reservaSchema = new Schema({
-    pista:   { type: Schema.Types.ObjectId, ref: 'Pista', required: true },
+    pista: { type: Schema.Types.ObjectId, ref: 'Pista', required: true },
     usuario: { type: Schema.Types.ObjectId, ref: 'Usuario', required: true },
-    fecha:   { type: Date, required: true },
+    fecha: { type: Date, required: true },
     horaInicio: { type: String, required: true },
-    horaFin:    { type: String, required: true },
+    horaFin: { type: String, required: true },
     estado: { type: String, enum: ['activa', 'cancelada'], default: 'activa' }
 }, { timestamps: true, versionKey: false });
 
